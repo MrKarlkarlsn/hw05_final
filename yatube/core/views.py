126e20821790591fd48b76836ac1d421c37b97a4
mac <mr.karlkarlsn@outlook.com>
2022-10-10 17:50:51 +0700

    5 ошибок осталось

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -2,9 +2,15 @@ from django.shortcuts import render
 
 
 def page_not_found(request, exception):
-    return render(request, 'core/404.html',
-                  {'path': request.path}, status=404)
+    context = {
+        'path': request.path,
+    }
+    return render(request, 'core/404.html', context, status=404)
 
 
 def csrf_failure(request, reason=''):
     return render(request, 'core/403csrf.html')
+
+
+def internal_server_error(request):
+    return render(request, 'core/500.html', status=500)
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*-*rnx+qc2@n94zql^*8wa4f2*%up%mn&so9si_34+du2&ixb+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',
